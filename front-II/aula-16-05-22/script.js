3e2d5d8861f622da228e10be7618977fe24f62ed
charrua950 <jrcf90@gmail.com>
2022-05-16 22:40:18 -0300

    Adicionado m pocuo de js

---
+++ front-II/aula-16-05-22/script.js
@@ -0,0 +1,13 @@
+let nome = 'Juan Costa'
+
+//Array
+let frutas = ['Banana', 'Sandia', 'Durazno']
+
+console.log(nome)
+console.log(frutas)
+
+//Recorrendo Array
+
+for (let fruta of frutas) {
+  console.log('La fruta actual es: ' + fruta)
+}
